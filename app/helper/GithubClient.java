949aa2a9b56e2f00e239b65d3fc4fb1db7044ddf
Keta Thakkar <ketathakkar@Ketas-MacBook-Pro.local>
2021-11-20 19:56:36 -0500

    Added javadoc

--- app/helper/GithubClient.java
+++ app/helper/GithubClient.java
@@ -15,6 +15,10 @@ import play.libs.ws.WSRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * This class makes requests to Github API to fetch content based on parameters.
+ */
+
 public class GithubClient implements WSBodyReadables, WSBodyWritables  {
 	
 	private WSClient wsClient;
@@ -31,6 +35,16 @@ public class GithubClient implements WSBodyReadables, WSBodyWritables  {
 	public WSClient getWsClient() {
 		return this.wsClient;
 	}
+	
+    /**
+     * <p>
+     * This method makes request to the <code>search</code> API of Github and
+     * fetches the top 10 repos for the <code>searchKey</code> sorted by date of upload.
+     * </p>
+     *
+     * @param searchKey this is the key for which search request is executed
+     * @return {@link CompletionStage} of {@link SearchResults}.
+     */
 	public CompletionStage<SearchResults> fetchRepos(String searchKey){
 		WSRequest request = this.wsClient
 		.url(BASE_URL + "search/repositories")
@@ -42,6 +56,17 @@ public class GithubClient implements WSBodyReadables, WSBodyWritables  {
 		return request.get().thenApply(wsResponse -> Json.parse(wsResponse.getBody())).thenApply(wsResponse -> Json.fromJson(wsResponse, SearchResults.class)).toCompletableFuture();
 
 	}
+	
+	 /**
+     * <p>
+     * This method makes request to the <code>repository</code> API of Github and
+     * fetches the details for the top 10 repos.
+     * </p>
+     *
+     * @param userName this is the userName for which search request is executed
+     * @param repoName this is the repoName for which repo request is executed
+     * @return {@link CompletionStage} of {@link RepoDetail}.
+     */
 
 	public CompletionStage<RepoDetail> fetchRepoDetail(String userName, String repoName) {
 		WSRequest request = this.wsClient
@@ -49,7 +74,18 @@ public class GithubClient implements WSBodyReadables, WSBodyWritables  {
 
 		return request.get().thenApply(wsResponse -> Json.parse(wsResponse.getBody())).thenApply(wsResponse -> Json.fromJson(wsResponse, RepoDetail.class)).toCompletableFuture();
 	}
-
+	
+	
+	 /**
+     * <p>
+     * This method makes request to the <code>issues</code> API of Github and
+     * fetches the top 10 issues for the repository.
+     * </p>
+     *
+     * @param userName this is the userName for which search request is executed
+     * @param repoName this is the repoName for which repo request is executed
+     * @return {@link CompletionStage} of {@link IssueItem}.
+     */
 	public CompletionStage<IssueItem[]> fetchIssues(String userName, String repoName) {
 		WSRequest request = this.wsClient
 		.url(BASE_URL + "repos/" + userName + "/" + repoName + "/issues")
@@ -57,7 +93,16 @@ public class GithubClient implements WSBodyReadables, WSBodyWritables  {
 
 		return request.get().thenApply(wsResponse -> Json.parse(wsResponse.getBody())).thenApply(wsResponse -> Json.fromJson(wsResponse, IssueItem[].class)).toCompletableFuture();
 	}
-
+	
+	 /**
+     * <p>
+     * This method makes request to the <code>topic</code> API of Github and
+     * fetches the top 10 topics for the repository.
+     * </p>
+     *
+     * @param topic this is the topic for which search request is executed
+     * @return {@link CompletionStage} of {@link SearchResults}.
+     */
 	public CompletionStage<SearchResults> fetchReposByTopic(String topic){
 		WSRequest request = this.wsClient
 		.url(BASE_URL + "search/repositories")
