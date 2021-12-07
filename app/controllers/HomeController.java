5db02477725aab55b17d2a0bd678c722a277f615
Keta Thakkar <ketathakkar@Ketas-MacBook-Pro.local>
2021-12-07 01:02:47 -0500

    Added websocket controller

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -6,15 +6,18 @@ import models.repoDetails.IssueItem;
 import models.repoDetails.RepoDetail;
 import models.*;
 import Businesslogic.*;
+import akka.actor.ActorSystem;
+import akka.stream.Materializer;
 import models.searchResult.SearchResults;
 import play.mvc.*;
+import service.GithubApi;
 import play.api.libs.json.Json;
+import play.libs.streams.ActorFlow;
 import play.libs.ws.WSClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -29,66 +32,88 @@ import java.util.Map;
  * <p>
  *
  * </p>
+ * 
  * @author Group Task
  */
 public class HomeController extends Controller {
-	
-	@Inject
+
+    @Inject
     WSClient wsClient;
+    @Inject
+    private ActorSystem actorSystem;
+    @Inject
+    private Materializer materializer;
+    GithubApi serviceImpl;
+    private final WSClient ws;
 
     GithubClient githubClient;
     userProfile profile = new userProfile();
     userRepo userRepo = new userRepo();
     final Logger logger = LoggerFactory.getLogger("play");
-   
 
     private static int counter = 1;
-
 
     /**
      * Controller Constructor
      */
-    public HomeController() {
+    // public HomeController() {
+    //     this.githubClient = new GithubClient();
+    // }
+
+    @Inject
+    public HomeController(ActorSystem system, WSClient ws, GithubApi serviceImpl) {
+        this.ws = ws;
+        this.serviceImpl = serviceImpl;
         this.githubClient = new GithubClient();
     }
 
-  
     /**
      * An action that renders an HTML page with a welcome message.
      * The configuration in the <code>routes</code> file means that
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
+     * 
      * @author Group Task
      */
-    public CompletionStage<Result> index(Http.Request request, String searchKeyword) 
-    {
-    	if (searchKeyword == "") {
-            if (!Session.isSessionExist(request)) {
-                counter += 1;
-                return CompletableFuture.completedFuture(ok(views.html.index.render(Session.getSearchResultsHashMapFromSession(request))).addingToSession(request, Session.getSessionKey(), Integer.toString(counter)));
-            } else {
-                return CompletableFuture.completedFuture(ok(views.html.index.render(Session.getSearchResultsHashMapFromSession(request))));
-            }
-    	}
-    	if (this.githubClient.getWsClient() == null) {
-            this.githubClient.setWsClient(wsClient);
-        }
-        logger.info(searchKeyword);
-        CompletionStage<SearchResults> response = this.githubClient.fetchRepos(searchKeyword);
-        return response.thenApply(resp -> {
-        
-            Session.setSessionSearchResultsHashMap(request, searchKeyword, resp);
-            if (!Session.isSessionExist(request)) {
-                counter += 1;
-                return ok(views.html.index.render(Session.getSearchResultsHashMapFromSession(request))).addingToSession(request, Session.getSessionKey(), Integer.toString(counter));
-            } else {
-                return ok(views.html.index.render(Session.getSearchResultsHashMapFromSession(request)));
-            }
-        });
+    public Result index(Http.Request request, String searchKeyword) {
+        // if (searchKeyword == "") {
+        //     if (!Session.isSessionExist(request)) {
+        //         counter += 1;
+        //         return CompletableFuture.completedFuture(
+        //                 ok(views.html.index.render(Session.getSearchResultsHashMapFromSession(request)))
+        //                         .addingToSession(request, Session.getSessionKey(), Integer.toString(counter)));
+        //     } else {
+        //         return CompletableFuture.completedFuture(
+        //                 ok(views.html.index.render(Session.getSearchResultsHashMapFromSession(request))));
+        //     }
+        // }
+        // if (this.githubClient.getWsClient() == null) {
+        //     this.githubClient.setWsClient(wsClient);
+        // }
+        // logger.info(searchKeyword);
+        // CompletionStage<SearchResults> response = this.githubClient.fetchRepos(searchKeyword);
+        // return response.thenApply(resp -> {
+
+        //     Session.setSessionSearchResultsHashMap(request, searchKeyword, resp);
+        //     if (!Session.isSessionExist(request)) {
+        //         counter += 1;
+        //         return ok(views.html.index.render(Session.getSearchResultsHashMapFromSession(request)))
+        //                 .addingToSession(request, Session.getSessionKey(), Integer.toString(counter));
+        //     } else {
+        //         return ok(views.html.index.render(Session.getSearchResultsHashMapFromSession(request)));
+        //     }
+        // });
+
+            return ok(views.html.index.render(request));
+
     }
 
     /**
-     * <p>With this function all the issues of the hyperlinked reppository can be fetched.</p>
+     * <p>
+     * With this function all the issues of the hyperlinked reppository can be
+     * fetched.
+     * </p>
+     * 
      * @param user It represents the github name of the user.
      * @param repo It represents the name of the repository.
      * @return It returns list of issues.
@@ -108,7 +133,10 @@ public class HomeController extends Controller {
     }
 
     /**
-     * <p> With this function all the topics of the repository can be fetched</p>
+     * <p>
+     * With this function all the topics of the repository can be fetched
+     * </p>
+     * 
      * @param topic It stores the topic.
      * @return Returns top 10 topics of the repository
      * @author Keta Thakkar
@@ -125,33 +153,41 @@ public class HomeController extends Controller {
     }
 
     /**
-     *<p>This controller is designed to search user's public github information and repositories</p>
-     * @param username it is used to make an api call to get the user's public github information and repositories
+     * <p>
+     * This controller is designed to search user's public github information and
+     * repositories
+     * </p>
+     * 
+     * @param username it is used to make an api call to get the user's public
+     *                 github information and repositories
      * @return html file with user's public github information and repositories
      * @author Aniket Tailor
      */
-    public CompletionStage<Result> getUserProfile(String username){
+    public CompletionStage<Result> getUserProfile(String username) {
         return CompletableFuture
                 .supplyAsync(() -> profile.getData(username))
                 .thenCombine(
                         CompletableFuture.supplyAsync(() -> userRepo.getData(username)),
-                        (userdata,repo) -> ok(views.html.publicInformation.render(userdata,repo))
-                );
+                        (userdata, repo) -> ok(views.html.publicInformation.render(userdata, repo)));
     }
-    
-    public Result stats() { 
-		 return ok(views.html.statistics.render()); 
-	}
-	
 
-	/*
-	 * public Result getStats(Http.Request request, String keyword) { if
-	 * (!Session.isSessionExist(request)) { return unauthorized("No Session Exist");
-	 * } if (Session.getSearchResultsHashMapFromSession(request) == null ||
-	 * Session.getSearchResultsHashMapFromSession(request).get(keyword) == null) {
-	 * return notFound(views.html.statistics.render(null)); } Map<String, Long>
-	 * similarityStatsMap = this.githubClient
-	 * .getSimilarityStats(Session.getSearchResultsHashMapFromSession(request),
-	 * keyword); return ok(views.html.statistics.render(similarityStatsMap)); }
-	 */
+    public Result stats() {
+        return ok(views.html.statistics.render());
+    }
+
+    // public WebSocket ws(){
+    // return WebSocket.Json.accept(request -> ActorFlow.actorRef(s ->
+    // GithubActor.props(s,wsClient), actorSystem, materializer));
+    // }
+
+    /*
+     * public Result getStats(Http.Request request, String keyword) { if
+     * (!Session.isSessionExist(request)) { return unauthorized("No Session Exist");
+     * } if (Session.getSearchResultsHashMapFromSession(request) == null ||
+     * Session.getSearchResultsHashMapFromSession(request).get(keyword) == null) {
+     * return notFound(views.html.statistics.render(null)); } Map<String, Long>
+     * similarityStatsMap = this.githubClient
+     * .getSimilarityStats(Session.getSearchResultsHashMapFromSession(request),
+     * keyword); return ok(views.html.statistics.render(similarityStatsMap)); }
+     */
 }
